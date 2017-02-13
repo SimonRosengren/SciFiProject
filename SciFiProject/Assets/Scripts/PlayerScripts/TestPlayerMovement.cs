b14ff9ac01ae901104fc8735aa6bba8e8ed0cc57
xzorcious <simonrosengren123@gmail.com>
2017-02-13 14:27:20 +0100

    Added a test room
    Test room with movable cube and following camera. Lag problems?

---
+++ SciFiProject/Assets/Scripts/PlayerScripts/TestPlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayerMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    Vector3 direction;
+
+    public float speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        transform.Translate(x, 0, z);
+    }
+}
